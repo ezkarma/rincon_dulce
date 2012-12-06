c3548821c232cf967ab512de48f77ea85303954f
ray <rayp03@hotmail.com>
2012-12-06 08:00:02 -0600

    final

--- modelo/Pedido.php
+++ modelo/Pedido.php
@@ -5,6 +5,13 @@ class Pedido{
 	var  $id_cliente;
 	var  $direccion;
 	
+	var $id_producto;
+	var $cantidad;
+	var $subtotal;
+	
+	var $nombre;
+	var $precio;
+	
   public static function cargar(){
 	$con = new Connection();
 	$con->start();
@@ -16,6 +23,26 @@ class Pedido{
 		$tmp->id_pedido = $row["id_pedido"];
 		$tmp->id_cliente = $row["id_cliente"];
 		$tmp->direccion = $row["direccion"];
+	
+	$pedidos[] = $tmp;
+		
+	}	
+	return $pedidos;
+	
+	}
+	
+	public static function cargar_detalle($id_pedido){
+	$con = new Connection();
+	$con->start();
+	
+	$res = mysql_query("select * from detalle_pedido,producto where detalle_pedido.id_pedido=1 and detalle_pedido.id_producto=producto.id_producto;");
+	$pedidos = array();
+	while($row = mysql_fetch_assoc($res)){
+		$tmp = new Pedido();
+		$tmp->nombre = $row["nombre"];
+		$tmp->precio = $row["precio"];
+		$tmp->cantidad = $row["cantidad"];
+		$tmp->subtotal = $row["subtotal"];
 	
 	$pedidos[] = $tmp;
 		
--- vista/pedidos.php
+++ vista/pedidos.php
@@ -7,13 +7,55 @@
 $pedidos=Pedido::cargar();
 
 foreach($pedidos as $pedido){
-echo $pedido->id_pedido;
+echo "Numero de Pedido: ".$pedido->id_pedido;
 echo "<br><br><br>";
 
+?>
+
+
+<table border='0' class='table table-striped'>
+
+<tr>
+		<th width='400'><font size='3'>Producto</font></th>
+		<th width='100'><font size='3'>Precio</font></th>
+		<th width='100'><center><font size='3'>Cantidad</font></center></th>
+		<th width='100'><center><font size='3'>Subtotal</font></center></th>
+		<th></th>
+</tr>
+
+<?php
+$detalles = Pedido::cargar_detalle($pedido->id_pedido);
+foreach($detalles as $detalle){
+
+
+$total=0;
+
+
+$subtotal=$detalle->precio*$detalle->cantidad;
+?>
+
+
+<tr>
+<td><?php echo $detalle->nombre;?></td>
+<td><?php echo $detalle->precio;?></td>
+<td><?php echo $detalle->cantidad;?></td>
+<td><?php echo $subtotal;?></td>
+
+<tr>
+
+<?php
+$total=$total+$subtotal;
 }
-	?>
+
+?>
+<td></td>
+<td></td>
+
+<td>Total:</td>
+<td><?php echo $total;?></td>
+
 	</table>
 	</p>
 </body>
-	
+<?php } ?>	
 </html>
